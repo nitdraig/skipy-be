249f062b5f471efa6b91b563e21d7d6b793df84a
Agustin-desktop <agustin2051@gmail.com>
2024-01-17 22:14:37 -0300

    [ok]

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import router from "././src/routes/linkRoutes.js";
+import router from "./src/routes/linkRoutes.js";
 
 dotenv.config();
 
