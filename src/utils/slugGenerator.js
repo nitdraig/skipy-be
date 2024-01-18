43c401edfe4630e1a09f45b78f2b832dca0c0334
Agustin-desktop <agustin2051@gmail.com>
2024-01-18 02:25:50 -0300

    1

--- src/utils/slugGenerator.js
+++ src/utils/slugGenerator.js
@@ -1,9 +1,14 @@
-import { customAlphabet } from "nanoid";
+// import { customAlphabet } from "nanoid";
 
-// Define el alfabeto que deseas para los slugs (por ejemplo, letras minúsculas y números)
-const alphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
+// // Define el alfabeto que deseas para los slugs (por ejemplo, letras minúsculas y números)
+// const alphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
 
-// Crea una función de generación de slug con nanoid
-const generateSlug = customAlphabet(alphabet, 6);
+// // Crea una función de generación de slug con nanoid
+// const generateSlug = customAlphabet(alphabet, 6);
 
+// export default generateSlug;
+function generateSlug() {
+  // Puedes utilizar bibliotecas como `shortid` o `nanoid`
+  return Math.random().toString(36).substring(2, 8);
+}
 export default generateSlug;
